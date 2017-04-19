7b4fe07dd8c617ca46060200dea7dc8c1d27b761
Koushik Dutta <koushd@gmail.com>
2017-04-18 20:59:20 -0700

    wip

--- app.js
+++ app.js
@@ -58,12 +58,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-
-var debug = require('debug')('allcast-site');
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
